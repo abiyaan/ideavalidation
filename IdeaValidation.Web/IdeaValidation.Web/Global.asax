0f839fc47b2404dc27a78da73d77c578e300ecdd
Amir Taj <amir.taj@islington.gov.uk>
2019-03-22 18:53:18 +0000

    Setting up the project

---
+++ IdeaValidation.Web/IdeaValidation.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IdeaValidation.Web.MvcApplication" Language="C#" %>
---
+++ IdeaValidation.Web/IdeaValidation.Web/Models/RequestParameters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace IdeaValidation.Web.Models
+{
+    public class RequestParameters
+    {
+        /// <summary>
+        /// User enters main keyword or term for analysis
+        /// </summary>
+        [Display(Name = "Keyword")]
+        public string Keyword { get; set; }
+
+        /// <summary>
+        /// List of countries to perform analysis
+        /// </summary>
+        public IEnumerable<string> Countries { get; set; }
+
+        /// <summary>
+        /// List of languages to perform analysis
+        /// </summary>
+        public IEnumerable<string> Languages { get; set; }
+
+        /// <summary>
+        /// List of free style parameters for users
+        /// </summary>
+        public Dictionary<string, string> Parameters { get; set; }
+    }
+}
